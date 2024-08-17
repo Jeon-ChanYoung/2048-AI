10db63886ab230a26ba2b2864346ac1d4dabe5ce
Jeon-ChanYoung <massmoab01@gmail.com>
2024-08-17 10:10:25 +0900

    DQN_model linear에서 cnn으로 변경

--- DQN_model.py
+++ DQN_model.py
@@ -5,40 +5,50 @@ import torch.optim as optim
 import numpy as np
 
 class DeepQNetwork(nn.Module):
-    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions):
+    def __init__(self, lr, input_dims, n_actions):
         super(DeepQNetwork, self).__init__()
-        self.fc1 = nn.Linear(*input_dims, fc1_dims)
-        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
-        self.fc3 = nn.Linear(fc2_dims, n_actions)
+        self.conv1 = nn.Conv2d(input_dims[0], 64, kernel_size=2, stride=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=2, stride=1)
+        
+        self.fc_input_dims = self.calculate_conv_output_dims(input_dims)
+        self.fc1 = nn.Linear(self.fc_input_dims, 256)
+        self.fc2 = nn.Linear(256, n_actions)
 
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.MSELoss()
-        
+
+    def calculate_conv_output_dims(self, input_dims):
+        state = T.zeros(1, *input_dims)
+        dims = self.conv1(state)
+        dims = self.conv2(dims)
+        return int(np.prod(dims.size()))
+
     def forward(self, state):
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        actions = self.fc3(x)
+        x = F.relu(self.conv1(state))
+        x = F.relu(self.conv2(x))
+        x = x.view(x.size()[0], -1)
+        x = F.relu(self.fc1(x))
+        actions = self.fc2(x)
 
         return actions
 
 class Agent():
     def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions,
-                 max_mem_size = 100000, eps_end = 0.01, eps_dec = 5e-4):
-        self.gamma = gamma 
+                 max_memory_size=100000, epsilon_min=0.01, epsilon_decay=5e-4):
+        self.gamma = gamma
         self.epsilon = epsilon
-        self.eps_min = eps_end
-        self.eps_dec = eps_dec
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
         self.lr = lr
         self.action_space = [i for i in range(n_actions)]
-        self.mem_size = max_mem_size
-        self.batch_size =  batch_size
+        self.mem_size = max_memory_size
+        self.batch_size = batch_size
         self.mem_cntr = 0
 
-        self.Q_eval = DeepQNetwork(self.lr, n_actions=n_actions, input_dims=input_dims,
-                                   fc1_dims=256, fc2_dims=256)
-        
+        self.Q_eval = DeepQNetwork(lr, input_dims=input_dims, n_actions=n_actions)
+
         self.state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)        
+        self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
         self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
         self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
         self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool_)
@@ -52,7 +62,7 @@ class Agent():
         self.terminal_memory[index] = done
 
         self.mem_cntr += 1
-
+    
     def choose_action(self, observation):
         if np.random.random() > self.epsilon:
             state = T.tensor([observation], dtype=T.float)
@@ -62,7 +72,7 @@ class Agent():
             action = np.random.choice(self.action_space)
         
         return action
-    
+
     def learn(self):
         if self.mem_cntr < self.batch_size:
             return 
@@ -89,4 +99,12 @@ class Agent():
         loss.backward()
         self.Q_eval.optimizer.step()
 
-        self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min else self.eps_min 
+        self.epsilon = self.epsilon - self.epsilon_decay if self.epsilon > self.epsilon_min else self.epsilon_min
+
+    def save_model(self, filename: str):
+        T.save(self.Q_eval.state_dict(), filename)
+        print(f"Model saved to {filename}")
+
+    def load_model(self, filename: str):
+        self.Q_eval.load_state_dict(T.load(filename))
+        print(f"Model loaded from {filename}")
