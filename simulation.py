1e559982e9b5877df1c66022a2eed8293367b978
Jeon-ChanYoung <massmoab01@gmail.com>
2024-08-21 21:53:16 +0900

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,28 +1,11 @@
-from Game2048 import Game2048GUI
+from Game2048 import Game2048Env, Game2048GUI
 from DQN_model import Agent
 
-gamma = 0.99 
-epsilon = 1.0 
-lr = 0.001
-input_dims = (1, 4, 4)  
-batch_size = 64
-n_actions = 4
-max_mem_size = 100000 
-epsilon_min = 0.01 
-epsilon_decay = 0.0001
-agent = Agent(gamma=gamma, 
-              epsilon=epsilon, 
-              lr=lr,
-              input_dims=input_dims,
-              batch_size=batch_size, 
-              n_actions=n_actions, 
-              max_memory_size=max_mem_size, 
-              epsilon_min=epsilon_min,
-              epsilon_decay=epsilon_decay)
-
+env = Game2048Env()
+agent = Agent(env)
 
 # 훈련된 모델 파일 경로
-model_filename = 'Learning_models/2048dqn_model_100.pth'
+model_filename = 'Learning_models/2048DQN_1000.pth'
 agent.load_model(model_filename)
 app = Game2048GUI(agent)
 app.mainloop()
