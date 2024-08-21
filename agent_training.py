19c4ac46e0ea8cf36fbd14fe740a0b23453f935b
Jeon-ChanYoung <massmoab01@gmail.com>
2024-08-21 21:51:16 +0900

    Update agent_training.py

--- agent_training.py
+++ agent_training.py
@@ -1,50 +1,27 @@
-from Game2048 import Game2048
-from DQN_model import Agent
+from Game2048 import *
+from DQN_model import *
 
-def train(agent:Agent, game:Game2048, n_games=100):
-    scores = []
-    for i in range(n_games):
-        done = False
-        state = game.get_state()
-        score = 0
+env = Game2048Env()
+agent = Agent(env)
+agent.load_model("Learning_models/2048DQN_1000.pth")
+num_episodes = 1000
+target_update_freq = 10
 
-        while not done:
-            action = agent.choose_action(state)
-            new_state, reward, done = game.step(action)
-            agent.store_transition(state, action, reward, new_state, done)
-            agent.learn()
-            state = new_state
-            score += reward
-        
-        scores.append(score)
-        game.__init__() 
-        print(f'| Game: {i} | Score: {score:.2f} | Epsilon: {agent.epsilon:.2f} |')
-
-    return scores
-
-
-gamma = 0.99 # 할인율 (미래 보상에 대한 중요도)
-epsilon = 1.0 # 앱실론
-lr = 0.001 # 학습률
-input_dims = (1, 4, 4)  # 2048 게임 보드의 상태 크기 (채널, 높이, 너비)
-batch_size = 64
-n_actions = 4 # 가능한 행동의 수 (2048 게임에서 상, 하, 좌, 우)
-max_mem_size = 100000 # 리플레이 메모리 최대 크기
-epsilon_min = 0.01 # 탐험률 최소값
-epsilon_decay = 0.0001
-agent = Agent(gamma=gamma, 
-              epsilon=epsilon, 
-              lr=lr,
-              input_dims=input_dims,
-              batch_size=batch_size, 
-              n_actions=n_actions, 
-              max_memory_size=max_mem_size, 
-              epsilon_min=epsilon_min,
-              epsilon_decay=epsilon_decay)
-
-# 게임 환경 생성
-game = Game2048()
-
-# 학습 진행
-train(agent, game)
-agent.save_model('2048dqn_model.pth')
+for episode in range(num_episodes):
+    state = env.reset()
+    total_reward = 0
+    done = False
+    
+    while not done:
+        action = agent.choose_action(state)
+        next_state, reward, done, _ = env.step(action)
+        agent.remember(state, action, reward, next_state, done)
+        agent.train()
+        state = next_state
+        total_reward += reward
+    
+    if episode % target_update_freq == 0:
+        agent.update_target()
+    
+    print(f"Episode: {episode}, Total Reward: {total_reward:.2f}, Epsilon: {agent.epsilon:.2f}")
+agent.save_model("2048DQN_2000.pth")
